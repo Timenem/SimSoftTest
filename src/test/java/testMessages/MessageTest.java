f42c1c9ba3f3970f852ee1cf55053cb1e2219843
Timenem <https://github.com/Timenem>
2021-10-19 16:22:02 +0300

    step3 ready

---
+++ src/test/java/testMessages/MessageTest.java
@@ -0,0 +1,18 @@
+package testMessages;
+
+import org.testng.annotations.Test;
+import testBase.BaseTest;
+
+import static constants.Constants.UrlConstants.LINK;
+
+@Test(description = "this test check messages quantity and send feedback")
+public class MessageTest extends BaseTest {
+    public void start() throws InterruptedException {
+        basePage.openPage(LINK);
+        loginPage.enterLogin();
+        loginPage.enterPassword();
+        mailPage.checkMessages();
+       // mailPage.checkMessages();
+    }
+
+}
